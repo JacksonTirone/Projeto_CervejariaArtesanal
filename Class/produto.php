7a13314155b0d182e1957d3449bffa0fc86d5a77
Jackson Tirone <tirone.contato@live.com>
2020-06-28 16:19:57 -0300

    Pasta das classes do projeto

---
+++ Class/produto.php
@@ -0,0 +1,13 @@
+<?php
+    class Produto {
+        public $idProduto;
+        public $nome;
+        public $descricao;
+        public $unidadeDePeso;
+        public $preco;
+        public $tipoCerveja;
+        public $porcentagemAlcoolica;
+        public $marca_idMarca;
+    }
+
+?>
